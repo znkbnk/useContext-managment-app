fca7cc376c140f99a90003de1d9bd5ff35414d1c
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-06-20 19:36:31 +0100

    Create TaskContext.js

---
+++ TaskContext.js
@@ -0,0 +1,24 @@
+//TaskContext.js
+import React, { createContext, useState } from 'react';
+
+export const TaskContext = createContext();
+
+export const TaskProvider = ({ children }) => {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const deleteTask = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  return (
+    <TaskContext.Provider value={{ tasks, addTask, deleteTask }}>
+      {children}
+    </TaskContext.Provider>
+  );
+};
